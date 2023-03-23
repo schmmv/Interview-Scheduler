ed31c5aeb7a8cccf05ced4cdf458f3ae01af03ee
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-22 20:21:33 -0600

    Move onClick function up to InterviewerList component to use id there

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -6,7 +6,7 @@ export default function InterviewerListItem(props) {
 
   const intClass = classNames('interviewers__item', { 'interviewers__item--selected': props.selected });
   return (
-    <li onClick={() => props.setInterviewer(props.id)}
+    <li onClick={props.setInterviewer}
     className={intClass}>
       <img
         className='interviewers__item-image'
