5ceb7b305b62016b7ad078be9d6b158fd6ac8428
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-24 11:05:30 -0600

    Add appointment and header stories

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
 
 
 storiesOf("Button", module)
@@ -146,3 +148,14 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+  /**
+   * Appointment stories
+   */
+  storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: 'white', value: '#fff', default: true }]
+  })
+  .add("Appointment", () => <Appointment/>)
+  .add("Appointment with Time", () => <Appointment time="12pm" />)
+  .add("Header", () => <Header time="12pm"/>)
