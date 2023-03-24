32730510f59a89bef444e0ccc28e5dfb457b3e89
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-03-24 16:45:03 -0600

    Add Form component for creating/editing an interview

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,44 @@
+import Button from "components/Button";
+import InterviewerList from "components/InterviewerList";
+import React, { useState } from "react";
+
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
+          <input
+            onChange={(event) => setStudent(event.target.value)}
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={student}
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
+          value={interviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
