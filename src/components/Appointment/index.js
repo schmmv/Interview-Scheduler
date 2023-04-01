a8cff077f18e8980e89d261086284fa76ba1d2fc
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-01 16:08:05 -0600

    Add save function to book a new appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,22 +3,37 @@ import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Status from './Status';
 import useVisualMode from 'hooks/useVisualMode';
 import Form from './Form';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING, true);
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    .catch((err) => console.log(err.response));
+  }
+
   return (
     <article className='appointment'>
       <Header time={props.time} />
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} />}
+      {mode === CREATE && <Form onSave={save} interviewers={props.interviewers} onCancel={back} />}
+      {mode === SAVING && <Status message="Saving..." />}
     </article>
   );
 }
