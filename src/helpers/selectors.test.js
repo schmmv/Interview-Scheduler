0b3f96e4364e0594a42be9d40a69489db0b9e9cd
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-05 12:49:44 -0600

    Add spots to test state data

--- src/helpers/selectors.test.js
+++ src/helpers/selectors.test.js
@@ -6,13 +6,15 @@ const state = {
       id: 1,
       name: "Monday",
       appointments: [1, 2, 3],
-      interviewers: [1, 2, 4]
+      interviewers: [1, 2, 4],
+      spots: 4
     },
     {
       id: 2,
       name: "Tuesday",
       appointments: [4, 5],
-      interviewers: [5, 10]
+      interviewers: [5, 10],
+      spots: 4
     }
   ],
   appointments: {
