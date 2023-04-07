10f8ec5f6ffcd2b830825d37ee39c97aaa8a327d
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-07 10:41:05 -0600

    Fix updating spots to not change when editing an interview

--- src/helpers/reducer.js
+++ src/helpers/reducer.js
@@ -1,42 +1,50 @@
 
 import { dayNumber } from "./selectors";
 
- export default function reducer(state, action) {
-    switch (action.type) {
+export default function reducer(state, action) {
+  switch (action.type) {
 
-      case "SET_DAY":
-        return { ...state, day: action.value };
+    case "SET_DAY":
+      return { ...state, day: action.value };
 
-      case "SET_APPLICATION_DATA":
+    case "SET_APPLICATION_DATA":
+      return { ...state, ...action.value };
 
-        return { ...state, ...action.value };
+    case "SET_INTERVIEW":
+      const appointment = {
+        ...state.appointments[action.value.id],
+        interview: action.value.interview ? { ...action.value.interview } : null
+      };
 
-      case "SET_INTERVIEW":
+      const appointments = {
+        ...state.appointments,
+        [action.value.id]: appointment,
+      }
 
-        const appointment = {
-          ...state.appointments[action.value.id],
-          interview: action.value.interview ? {...action.value.interview } : null
-        };
-      
-        const appointments = {
-          ...state.appointments,
-          [action.value.id]: appointment,
+      const days = [...state.days];
+      const existingInterview = state.appointments[action.value.id].interview;
+      // const spots = state.days[dayNumber(state.day)].spots;
+      if (!existingInterview) {
+        if (appointment.interview) {
+          days[dayNumber(state.day)].spots -= 1;
         }
+      } else {
+        if (!appointment.interview) {
+          days[dayNumber(state.day)].spots += 1;
+        }
+      }
+      // const day = {
+      //   ...state.days[dayNumber(state.day)],
+      //   spots: appointment.interview ? spots - 1 : spots + 1
+      // };
 
-        const spots = state.days[dayNumber(state.day)].spots;
+      // days[dayNumber(state.day)] = day;
 
-        const day = {
-          ...state.days[dayNumber(state.day)],
-          spots: appointment.interview ? spots - 1 : spots + 1
-        };
+      return { ...state, appointments, days };
 
-        const days = state.days;
-        days[dayNumber(state.day)] = day;
-
-        return {...state, appointments, days };
-      default:
-        throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
-    }
+    default:
+      throw new Error(`Tried to reduce with unsupported action type: ${action.type}`);
   }
+}
 
 
