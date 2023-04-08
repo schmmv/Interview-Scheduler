c45ef8e28e1f59f827b27c51d367afaccfb8e8b8
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-07 19:04:09 -0600

    Connect to webSocket Server

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -3,9 +3,6 @@ import axios from 'axios';
 import { ACTION } from '../constants';
 import reducer from "../helpers/reducer";
 
-
-
-
 export default function useApplicationData() {
 
   const [state, dispatch] = useReducer(reducer, {
@@ -23,41 +20,50 @@ export default function useApplicationData() {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      dispatch({ type: ACTION.SET_APPLICATION_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data} });
+      dispatch({ type: ACTION.SET_APPLICATION_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data } });
     })
-    .catch((error) => {
-      console.log(error);
-    });
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
-  //WebSocket 
-  // useEffect(() => {
-  //   const socket = new WebSocket('ws://localhost:8001');
-  //   socket.onopen = (event) => {
-  //     socket.send("Sending text from client to server");
-  //   };
-  //   const cleanup = () => {
-  //     socket.close();
-  //   }
-    
-  // }, []);
+  // WebSocket 
+  useEffect(() => {
+    const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    socket.onopen = (event) => {
+      socket.send("ping");
+
+      socket.onmessage = event => {
+        const msg = JSON.parse(event.data);
+        console.log("Message Received From Server:", msg);
+        if (msg.type === "SET_INTERVIEW") {
+          console.log("dispatching set interview");
+          dispatch({ type: ACTION.SET_INTERVIEW, value: { id: msg.id, interview: msg.interview }});
+        }
+      };
+    };
+    const cleanup = () => {
+      socket.close();
+    }
+    return cleanup;
+  }, []);
 
   function bookInterview(id, interview) {
 
-   return axios.put(`/api/appointments/${id}`, {
-        interview
-      })
+    return axios.put(`/api/appointments/${id}`, {
+      interview
+    })
       .then(() => {
-        dispatch({ type: ACTION.SET_INTERVIEW, value: { id, interview }});
+        dispatch({ type: ACTION.SET_INTERVIEW, value: { id, interview } });
       });
   }
 
   function cancelInterview(id) {
 
-   return axios.delete(`/api/appointments/${id}`)
-    .then(() => {
-      dispatch({ type: ACTION.SET_INTERVIEW, value: { id, interview: null }});
-    });
+    return axios.delete(`/api/appointments/${id}`)
+      .then(() => {
+        dispatch({ type: ACTION.SET_INTERVIEW, value: { id, interview: null } });
+      });
   }
 
   return { state, setDay, bookInterview, cancelInterview };
