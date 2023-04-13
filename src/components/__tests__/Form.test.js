53ecc58f25decc5f87b8dfc9dc53a00a799c65eb
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-12 20:09:02 -0600

    Fix: Move declarations outside the test into the first 2 tests

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -16,14 +16,15 @@ describe("Form", () => {
     }
   ];
   
-  const { getByPlaceholderText } = render(<Form interviewers={interviewers}/>);
-  const { getByTestId } = render(<Form interviewers={interviewers} name="Lydia Miller-Jones" />);
-
   it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(<Form interviewers={interviewers}/>);
+
     expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
   });
-
+  
   it("renders with initial student name", () => {
+    const { getByTestId } = render(<Form interviewers={interviewers} student="Lydia Miller-Jones" />);
+
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
