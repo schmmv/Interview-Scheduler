876480a6d83e0ca6c84724f9bd818615643cb4b0
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-15 15:31:13 -0600

    Test that it loads data and books an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
+
+describe("Application", () => {
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
@@ -26,3 +28,38 @@ it("changes the schedule when a new day is selected", async () => {
   expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
  */
+
+it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  /**
+   * Render the Application.
+   
+Wait until the text "Archie Cohen" is displayed.
+Click the "Add" button on the first empty appointment.
+Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+Click the first interviewer in the list.
+Click the "Save" button on that same appointment.
+Check that the element with the text "Saving" is displayed.
+Wait until the element with the text "Lydia Miller-Jones" is displayed.
+Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+   */
+  //Render the Application
+  const { container, debug } = render(<Application />);
+  //Wait until the text "Archie Cohen" is displayed
+  await waitForElement(() => getByText(container, 'Archie Cohen'));
+  const appointments = getAllByTestId(container, "appointment");
+  const appointment = appointments[0];
+  console.log("First found appointment by TestId: ", prettyDOM(appointment));
+  //Click the "Add" button on the first empty appointment.
+  fireEvent.click(getByAltText(appointment, 'Add'));
+  //Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), { target: { value: "Lydia Miller-Jones" } });
+  //Click the first interviewer in the list.
+  fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
+  //Click the "Save" button on that same appointment.
+  fireEvent.click(getByText(appointment, 'Save'));
+  expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+  await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+  console.log(prettyDOM(getAllByTestId(container, "appointment")));
+})
+
+});
