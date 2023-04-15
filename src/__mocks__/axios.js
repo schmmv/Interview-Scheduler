eb491b6a52d9217d5a6cff3f311eb41371b792fb
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-15 15:31:31 -0600

    Add mock axios put request

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -78,5 +78,8 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+  put: jest.fn(url => {
+    return Promise.resolve({ status: 204, statusText: "No Content" });
   })
 }
