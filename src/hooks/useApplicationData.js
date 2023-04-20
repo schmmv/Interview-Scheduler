ccc605d3bf0863ad291afe1bea17f42343f1f79e
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-19 20:05:36 -0600

    Format and comment code, remove old comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,68 +1,80 @@
 import { useEffect, useReducer } from "react";
-import axios from 'axios';
-import { ACTION } from '../constants';
+import axios from "axios";
+import { ACTION } from "../constants";
 import reducer from "../reducers/application";
 
+/**
+ * Custom Hook for loading initial data and dispatching actions to update state
+ */
 export default function useApplicationData() {
-
   const [state, dispatch] = useReducer(reducer, {
     day: "Monday",
     days: [],
     appointments: {},
-    interviewers: {}
+    interviewers: {},
   });
 
-  const setDay = day => dispatch({ type: ACTION.SET_DAY, value: day });
+  //set day that is clicked on
+  const setDay = (day) => dispatch({ type: ACTION.SET_DAY, value: day });
 
+  //get data upon render and set state to that data
   useEffect(() => {
     Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((all) => {
-      dispatch({ type: ACTION.SET_APPLICATION_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data } });
-    })
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ])
+      .then((all) => {
+        dispatch({
+          type: ACTION.SET_APPLICATION_DATA,
+          value: {
+            days: all[0].data,
+            appointments: all[1].data,
+            interviewers: all[2].data,
+          },
+        });
+      })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  // WebSocket 
+  //WebSocket for persistent connection to scheduler API server
+  //Real-time updates between multiple users
   useEffect(() => {
     const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
     socket.onopen = (event) => {
-      // socket.send("ping");
-
-      socket.onmessage = event => {
+      socket.onmessage = (event) => {
         const msg = JSON.parse(event.data);
-        // console.log("Message Received From Server:", msg);
         if (msg.type === "SET_INTERVIEW") {
-          dispatch({ type: ACTION.SET_INTERVIEW, value: { id: msg.id, interview: msg.interview } });
+          dispatch({
+            type: ACTION.SET_INTERVIEW,
+            value: { id: msg.id, interview: msg.interview },
+          });
         }
       };
     };
     const cleanup = () => {
       socket.close();
-    }
+    };
     return cleanup;
   }, []);
 
+  /**
+   * Add an interview (object) based on appointment slot id
+   * @returns axios put request promise
+   */
   function bookInterview(id, interview) {
-
     return axios.put(`/api/appointments/${id}`, {
-      interview
-    })
-  //     .then(() => {
-  //       dispatch({ type: ACTION.SET_INTERVIEW, value: { id, interview } });
-  //     });
- }
-
+      interview,
+    });
+  }
+  /**
+   * Delete an interview based on appointment slot id
+   * @returns axios delete request promise
+   */
   function cancelInterview(id) {
-
-    return axios.delete(`/api/appointments/${id}`)
-      // .then(() => {
-      //   dispatch({ type: ACTION.SET_INTERVIEW, value: { id, interview: null } });
-      // });
+    return axios.delete(`/api/appointments/${id}`);
   }
 
   return { state, setDay, bookInterview, cancelInterview };
