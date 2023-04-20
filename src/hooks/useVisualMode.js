d271fd330c2e82ade614a56a1ee14f4a7a2b0a2b
Michele Schulz <michelecaleb.schulz@gmail.com>
2023-04-19 20:04:59 -0600

    Format and comment code, remove old comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,24 +1,27 @@
 import { useState } from "react";
-
+/**
+ * Custom Hook to keep track of appointment component mode and transition between them
+ */
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode, replace = false) {
+  //Transition to a different mode
+  const transition = function (newMode, replace = false) {
     setMode(newMode);
     if (!replace) {
       setHistory([...history, newMode]);
     }
-  }
-
-  const back = function() {
+  };
+  //Return to previous mode
+  const back = function () {
     if (history.length > 1) {
       const copyHistory = [...history];
       copyHistory.pop();
       setHistory(copyHistory);
       setMode(copyHistory[copyHistory.length - 1]);
     }
-  }
+  };
 
   return { mode, transition, back };
 }
